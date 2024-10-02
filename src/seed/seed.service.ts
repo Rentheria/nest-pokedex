ccb63c3aace9271b0d8c34090dee0ce879c1c4c5
Manuel Alejandro Hernandez Renteria <manuel.hr@globant.com>
2024-10-02 11:01:31 -0600

    Multiple Insertions on Seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,6 +4,7 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { PokeInsert } from './interfaces/poke-insert.interface';
 
 @Injectable()
 export class SeedService {
@@ -15,15 +16,20 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=100',
     );
+    const pokemonToInsert: PokeInsert[] = [];
 
     data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no });
     });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
     return `Seed executed`;
   }
 }
